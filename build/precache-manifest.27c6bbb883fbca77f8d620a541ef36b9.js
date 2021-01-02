c27ab04edd2175be9db2f1af2582929f16dcda78
koke <mamechips@hotmail.com>
2021-01-03 00:38:00 +0900

    buildを追加

---
+++ build/precache-manifest.27c6bbb883fbca77f8d620a541ef36b9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468211a1f39592ca8a35c8770ca4af84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6bf27456749ddc1af1a0",
+    "url": "./static/css/main.02d88f17.chunk.css"
+  },
+  {
+    "revision": "0fc8bf727aef77f3411c",
+    "url": "./static/js/2.080ffb24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.080ffb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf27456749ddc1af1a0",
+    "url": "./static/js/main.a87c17ff.chunk.js"
+  },
+  {
+    "revision": "a5f0087032484c5da322",
+    "url": "./static/js/runtime-main.f6867962.js"
+  },
+  {
+    "revision": "cbdcd3c1e41b8454231cbf4c65ea6c3e",
+    "url": "./static/media/Apple Chancery.cbdcd3c1.woff"
+  },
+  {
+    "revision": "194efb7bd95b507bb62fb93d73f9f24e",
+    "url": "./static/media/Avenir.194efb7b.woff"
+  },
+  {
+    "revision": "0fd19c6b2976626a46719763c168bdd0",
+    "url": "./static/media/JuliusSansOne-Regular.0fd19c6b.woff"
+  },
+  {
+    "revision": "55d0f0d0a30565133c4ed82242d6a30b",
+    "url": "./static/media/concept-calm.55d0f0d0.jpg"
+  },
+  {
+    "revision": "1733726f4a206a6229672e5c955f5f8b",
+    "url": "./static/media/concept-happiness.1733726f.jpg"
+  },
+  {
+    "revision": "9354189238288230091fc1854c047a8d",
+    "url": "./static/media/concept-silence.93541892.jpg"
+  },
+  {
+    "revision": "8373ad5eb103a9e57d14abefecb2e73e",
+    "url": "./static/media/frame-menu_title_lost.8373ad5e.svg"
+  },
+  {
+    "revision": "395cd27a9b0575aa706d08d2dbb3f203",
+    "url": "./static/media/menu-pasta@2x.395cd27a.jpg"
+  },
+  {
+    "revision": "157b90933c7617c78c6373eab73b6502",
+    "url": "./static/media/thumb-menu_pasta.157b9093.jpg"
+  },
+  {
+    "revision": "ddc781ac909ae09a780c5f911fe47980",
+    "url": "./static/media/title_access.ddc781ac.svg"
+  },
+  {
+    "revision": "9c72ca624ec583405576423979589281",
+    "url": "./static/media/title_concept.9c72ca62.svg"
+  },
+  {
+    "revision": "0d669764cdd1bfb003ca015c56b27583",
+    "url": "./static/media/title_menu.0d669764.svg"
+  },
+  {
+    "revision": "7aa90532f1460d54ede46861aa207b3c",
+    "url": "./static/media/title_news.7aa90532.svg"
+  },
+  {
+    "revision": "60d7260acf33ac1034116cc06c19ed83",
+    "url": "./static/media/top-header.60d7260a.jpg"
+  },
+  {
+    "revision": "b5f31478b32f92711edcba3822220b1a",
+    "url": "./static/media/top_image.b5f31478.svg"
+  }
+]);
