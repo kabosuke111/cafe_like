786a685bb4efa8917a7a0fa8ac7d7695ee52c3f7
koke <mamechips@hotmail.com>
2021-01-07 02:25:55 +0900

    スマートホン対応中　コンセプト、メニュー

--- src/page/top/top_concept.tsx
+++ src/page/top/top_concept.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ImageCalm from '../../images/top/concept-calm.jpg';
 import ImageSilence from '../../images/top/concept-silence.jpg';
 import ImageHappiness from '../../images/top/concept-happiness.jpg';
+import ImageCalmSp2x from '../../images/top/2x/sp-concept-calm@2x.jpg';
+import ImageSilenceSp2x from '../../images/top/2x/sp-concept-silence@2x.jpg';
+import ImageHappinessSp2x from '../../images/top/2x/sp-concept-happiness@2x.jpg';
+import ImageCalmSp from '../../images/top/sp-concept-calm.jpg';
+import ImageSilenceSp from '../../images/top/sp-concept-silence.jpg';
+import ImageHappinessSp from '../../images/top/sp-concept-happiness.jpg';
 import TitleImage from './parts_image/title/title_image';
 import {upperCase} from './../../data/common';
 
 interface TYPE_concept_list {
-  "title": string,
-  "image": string
+  "title": string;
+  "image": string;
+  "sp2x": string;
+  "sp": string;
 }
 
 const TopConcept = () => {
@@ -16,14 +24,20 @@ const TopConcept = () => {
     {
       "title": "calm",
       "image": ImageCalm,
+      "sp2x": ImageCalmSp2x,
+      "sp": ImageCalmSp
     },
     {
       "title": "silence",
       "image": ImageSilence,
+      "sp2x": ImageSilenceSp2x,
+      "sp": ImageSilenceSp
     },
     {
       "title": "happiness",
       "image": ImageHappiness,
+      "sp2x": ImageHappinessSp2x,
+      "sp": ImageHappinessSp
     }
   ]
 
@@ -34,7 +48,10 @@ const TopConcept = () => {
         <ul className="b--concept__features m_d_flex_nowrap_stretch m_f_between">
           {conseptList.map((key) => (
               <li className="b--concept__features__item m_relative">
-                <img className={`b--concept__features__image ${key.title}`} src={key.image} alt={key.title} />
+                <picture>
+                  <source className={`b--concept__features__image ${key.title}`} media="(max-width: 768px)" srcSet={`${key.sp2x} 2x, ${key.sp} 1x`} />
+                  <img className={`b--concept__features__image ${key.title}`} src={key.image} alt={key.title} />
+                </picture>
                 <p className="b--concept__features__title m_font_white m_l_h_1">{`${upperCase(key.title)}`}</p>
               </li>
           ))}
